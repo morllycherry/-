c0ac23a0e2ebeb435b5a7de9d2682d8edfa56290
morllycherry <72075438+morllycherry@users.noreply.github.com>
2021-05-26 19:03:03 +0800

    Update verificationpart.py

--- verificationpart.py
+++ verificationpart.py
@@ -7,15 +7,15 @@ verification part 1
 def verification1():
     print("part 1")
     print("main idea is test")
-    print("wellcome to test server")
+    print("wellcome boy this is a new place")
     none = True
     while none:
 
-        part1 = input(str("please type your invention code:"))
+        part1 = input(str("please type your code:"))
         if part1 == "0":
             print("successful")
             none = False
         else:
-            print("unknow code")
+            print("unknow")
 
 verification1()
